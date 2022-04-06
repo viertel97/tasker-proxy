6d8c386f68779bd9395b36000ad086a3f405f540
viertel97 <janik.viertel@outlook.com>
2022-04-06 10:46:35 +0200

    added extensive logging

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import os
 import platform
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import APIRouter, Depends, FastAPI, Request
 from loguru import logger
 
 import api_objects
 import helper
-from apis import TODOIST_API
 from habit_tracker import (
     track_habit,
     track_mediation_habit,
@@ -16,11 +15,11 @@ from habit_tracker import (
 )
 from proxy import todoist_proxy
 
-app = FastAPI()
+app = FastAPI(debug=True)
+router = APIRouter()
 
 proxy_mapping_dict = helper.get_config("tasker_mapping.json")
 habit_tracker_mapping_dict = helper.get_config("habit_tracker_mapping.json")
-
 
 logger.add(
     os.path.join(os.path.dirname(os.path.abspath(__file__)) + "/logs/" + os.path.basename(__file__) + ".log"),
@@ -30,17 +29,31 @@ logger.add(
 )
 
 
+async def log_request_info(request: Request):
+    logger.debug(f"{request.method} {request.url}")
+    logger.debug("Params:")
+    for name, value in request.path_params.items():
+        logger.debug(f"\t{name}: {value}")
+    logger.debug("Headers:")
+    for name, value in request.headers.items():
+        logger.debug(f"\t{name}: {value}")
+    logger.debug("Body:")
+    request_body = await request.json()
+    for key in request_body.keys():
+        logger.debug(f"\t{key}: {request_body[key]}")
+
+
 @logger.catch
-@app.post("/proxy/{service}")
-def proxy(service: str):
+@router.post("/proxy/{service}")
+async def proxy(service: str):
     logger.info("service: " + service)
     selected_service = proxy_mapping_dict[service]
     todoist_proxy(selected_service)
 
 
 @logger.catch
-@app.post("/habit-tracker/sport/{service}")
-def habit_tracker(service: str):
+@router.post("/habit-tracker/sport/{service}")
+async def habit_tracker(service: str):
     logger.info("habit-tracker: " + service)
     selected_service = habit_tracker_mapping_dict[service]
     track_habit(selected_service)
@@ -49,26 +62,29 @@ def habit_tracker(service: str):
 
 
 @logger.catch
-@app.post("/habit-tracker/meditation")
-def test(item: api_objects.meditation_session):
+@router.post("/habit-tracker/meditation")
+async def test(item: api_objects.meditation_session):
     track_mediation_habit(item)
 
 
 @logger.catch
-@app.post("/habit-tracker/reading")
-def test(item: api_objects.reading_session):
+@router.post("/habit-tracker/reading")
+async def test(item: api_objects.reading_session):
     track_reading_habit(item)
 
 
 @logger.catch
-@app.post("/habit-tracker/timer")
-def test(item: api_objects.timer):
+@router.post("/habit-tracker/timer")
+async def test(item: api_objects.timer):
     track_time(item)
 
 
 @app.get("/proxy")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
+
+
+app.include_router(router, dependencies=[Depends(log_request_info)])
 
 
 if __name__ == "__main__":

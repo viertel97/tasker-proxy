c8acb29977b26443cdc3595e4a5f3db961e03d7d
viertel97 <janik.viertel@outlook.com>
2024-10-11 21:11:39 +0200

    added creation of OneNote notes from Obsidian

--- controller/obsidian_controller.py
+++ controller/obsidian_controller.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 import pandas as pd
 from dateutil.parser import parse
@@ -10,11 +11,12 @@ from quarter_lib.logging import setup_logging
 from quarter_lib_old.database import create_monica_server_connection
 
 from config.queries import activity_query
+from helper.web_helper import get_onenote_default_ids
+from services.microsoft_service import duplicate_page, get_new_page_id, get_access_token, add_formatted_text_to_page
 
 DEFAULT_ACCOUNT_ID = 1
 INBOX_CONTACT_ID = 52
 TO_DELETE_LIST = ["Inbox", "Blocker", "No-GitHub"]
-connection = create_monica_server_connection()
 
 logger = setup_logging(__name__)
 router = APIRouter(prefix="/obsidian", tags=["obsidian"])
@@ -67,3 +69,54 @@ async def get_activities_post(request: Request):
     result = get_activities(happened_at)
     prepared_result = prepare_result(result)
     return JSONResponse(content=prepared_result, status_code=status.HTTP_200_OK)
+
+
+def retry_get_new_page_id(duplicate_response, access_token, max_retries=10, backoff_factor=1, initial_delay=5):
+    retries = 0
+    delay = initial_delay
+    new_page_id = None
+
+    while new_page_id is None and retries < max_retries:
+        try:
+            new_page_id = get_new_page_id(duplicate_response, access_token)
+            if new_page_id is not None:
+                return new_page_id
+        except Exception as e:
+            logger.error(f"Error getting new page id: {e}")
+            pass
+
+        retries += 1
+        time.sleep(delay)
+        delay *= backoff_factor  # Exponential backoff
+
+    return JSONResponse(content={"error": "Could not get new page id"},
+                        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@router.post("/to_onenote")
+async def send_to_onenote(request: Request):
+    raw_body = await request.body()
+    body_data = json.loads(raw_body.decode("utf-8"))
+    happened_at = parse(body_data["happened_at"])
+
+    onenote_ids = get_onenote_default_ids()
+    access_token = get_access_token()
+    duplicate_response = duplicate_page(onenote_ids['default_template_page_id'],
+                                        onenote_ids['default_template_section_id'], access_token)
+    if duplicate_response is None:
+        return JSONResponse(content={"error": "Could not duplicate page"},
+                            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    logger.info("Duplicate response: " + str(duplicate_response))
+
+    new_page_id = retry_get_new_page_id(duplicate_response, access_token)
+
+    logger.info(f"New page id: {new_page_id}")
+    title = f"{happened_at.strftime('%Y-%m-%d')} - {body_data['summary']}"
+    logger.info(f"Adding to page {new_page_id} with title {title} - waiting 10 seconds first")
+    time.sleep(10)
+    add_text_response = add_formatted_text_to_page(new_page_id, body_data["content"], title, access_token)
+    if add_text_response is not None:
+        return JSONResponse(content={"page_id": new_page_id}, status_code=status.HTTP_200_OK)
+    else:
+        return JSONResponse(content={"error": "Could not add text to page"},
+                            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- helper/web_helper.py
+++ helper/web_helper.py
@@ -6,24 +6,22 @@ from helper.caching import ttl_cache
 
 logger = setup_logging(__file__)
 
-
-MASTER_KEY, HABIT_TRACKER_MAPPING_BIN, TASKER_MAPPING_BIN, REWORK_EVENTS_BIN = (
-    get_secrets(
-        [
-            "jsonbin/masterkey",
-            "jsonbin/habit_tracker_mapping-bin",
-            "jsonbin/tasker_mapping-bin",
-            "jsonbin/Rework-Events-bin",
-        ]
-    )
+MASTER_KEY, HABIT_TRACKER_MAPPING_BIN, TASKER_MAPPING_BIN, REWORK_EVENTS_BIN, ONENOTE_PAGES_BIN = get_secrets(
+    [
+        "jsonbin/masterkey",
+        "jsonbin/habit_tracker_mapping-bin",
+        "jsonbin/tasker_mapping-bin",
+        "jsonbin/Rework-Events-bin",
+        "jsonbin/OneNote-Pages-bin",
+    ]
 )
-
 
 BASE_URL = "https://api.jsonbin.io/v3"
 
 HABIT_TRACKER_MAPPING_URL = f"{BASE_URL}/b/{HABIT_TRACKER_MAPPING_BIN}/latest"
 TASKER_MAPPING_URL = f"{BASE_URL}/b/{TASKER_MAPPING_BIN}/latest"
 REWORK_EVENTS_URL = f"{BASE_URL}/b/{REWORK_EVENTS_BIN}/latest"
+ONENOTE_PAGES_URL = f"{BASE_URL}/b/{ONENOTE_PAGES_BIN}/latest"
 
 
 @ttl_cache(ttl=60 * 60)
@@ -51,6 +49,16 @@ def get_tasker_mapping_from_web():
     logger.info("getting habits from web")
     response = requests.get(
         TASKER_MAPPING_URL,
+        headers={"User-Agent": "Mozilla/5.0", "X-Master-Key": MASTER_KEY},
+    )
+    return response.json()["record"]
+
+
+@ttl_cache(ttl=60 * 60 * 24)
+def get_onenote_default_ids():
+    logger.info("getting onenote pages")
+    response = requests.get(
+        ONENOTE_PAGES_URL,
         headers={"User-Agent": "Mozilla/5.0", "X-Master-Key": MASTER_KEY},
     )
     return response.json()["record"]
